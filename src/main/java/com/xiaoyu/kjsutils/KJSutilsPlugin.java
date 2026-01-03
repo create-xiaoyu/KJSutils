b4435018563885a88fe81db258e6ccce0c0ba8cc
create-xiaoyu <abc18873060306@qq.com>
2026-01-03 16:14:37 +0800

    add Events for Player LongedIn or LongedOut Dimension

--- src/main/java/com/xiaoyu/kjsutils/KJSutilsPlugin.java
+++ src/main/java/com/xiaoyu/kjsutils/KJSutilsPlugin.java
@@ -1,13 +1,11 @@
 package com.xiaoyu.kjsutils;
 
-import dev.latvian.mods.kubejs.event.EventGroup;
+import com.xiaoyu.kjsutils.event.KJSutilEvents;
 import dev.latvian.mods.kubejs.event.EventGroupRegistry;
 import dev.latvian.mods.kubejs.plugin.KubeJSPlugin;
 import dev.latvian.mods.kubejs.script.BindingRegistry;
 
 public class KJSutilsPlugin implements KubeJSPlugin {
-    public static final EventGroup GROUP = EventGroup.of("KJSutils");
-
     @Override
     public void registerBindings(BindingRegistry bindings) {
         bindings.add("KJSutils", new KJSutilsWrapper());
@@ -15,6 +13,6 @@ public class KJSutilsPlugin implements KubeJSPlugin {
 
     @Override
     public void registerEvents(EventGroupRegistry registry) {
-        registry.register(GROUP);
+        registry.register(KJSutilEvents.GROUP);
     }
 }
